5e6fc3247631b0db04fdb8e9323a28fc09795738
peter daemonna ducai <peter.ducai@gmail.com>
2022-10-04 15:59:44 +0200

    runner

--- Dockerfile
+++ Dockerfile
@@ -4,13 +4,12 @@ FROM registry.access.redhat.com/ubi9:9.0.0-1640
 RUN dnf install fio util-linux python-pip wget -y && dnf clean all -y
 
 WORKDIR /
-COPY etcd.sh /
+COPY etcd-mg.sh /
 COPY fio_suite.sh /
-COPY fio_suite2.sh /
+
 COPY runner.sh /usr/local/bin/
 RUN chmod +x /fio_suite.sh
-RUN chmod +x /fio_suite2.sh
-RUN chmod +x /etcd.sh
+RUN chmod +x /etcd-mg.sh
 RUN chmod +x /usr/local/bin/runner.sh
 CMD ["/usr/local/bin/runner.sh"]
 ENTRYPOINT ["/usr/local/bin/runner.sh"]
---
+++ runner.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+case "$1" in
+  etcd)
+    ./etcd-mg.sh $2 $3
+    ;;
+  toolong)
+    etcd_tooktoolong.py $2
+    ;;
+  fio)
+    ./fio_suite.sh
+    ;;
+  full)
+    ./iostat.sh &
+    ./top.sh &
+    sleep 1
+    ./fio_suite2.sh &  > fio.log
+    wait
+    echo "All tests are complete"
+    echo -e ""
+    echo -e "RESULTS:"
+    cat top.log
+    echo -e ""
+    cat iostat.log
+    echo -e ""
+    cat r70_w30_1G_d4.log
+    echo -e ""
+    cat r70_w30_200M_d4.log
+    echo -e ""
+    cat r30_w70_200M_d1.log
+    echo -e ""
+    cat r30_w70_1G_d1.log
+    echo -e ""
+    cat r70_w30_1G_d4.log
+    echo -e ""
+    cat r30_w70_200M_d1.log
+    echo -e ""
+    cat r30_w70_1G_d1.log
+    #
+    rm r70_w30_1G_d4.log
+    rm r70_w30_200M_d4.log
+    rm r30_w70_200M_d1.log
+    rm r30_w70_1G_d1.log
+    rm r70_w30_1G_d4.log
+    rm r30_w70_200M_d1.log
+    rm r30_w70_1G_d1.log
+    ;;
+  *)
+    echo -e "NO PARAMS. Choose 'etcd' or 'fio' or 'toolong'"
+    ;;
+esac
