276ad0270c1368b26161ae31f9a58be1281c0579
peter daemonna ducai <peter.ducai@gmail.com>
2022-11-23 14:43:26 +0100

    olderthan variable

--- cleanup-analyzer.sh
+++ cleanup-analyzer.sh
@@ -1,33 +1,49 @@
 #!/bin/bash
 
+################################################################################
+#                            Cleanup Analyzer                                  #
+#                                                                              #
+# Script to analyze which Deployments, Images and ReplicaSets could be         #
+# cleaned up and to generate cleanup scripts.                                  #
+#                                                                              #
+#                                                                              #
+################################################################################
+################################################################################
+################################################################################
+#                                                                              #
+#  Copyright (C) 2022 Peter Ducai                                              #
+#  peter.ducai@gmail.com                                                       #
+#  pducai@icloud.com                                                           #
+#                                                                              #
+#  This program is free software; you can redistribute it and/or modify        #
+#  it under the terms of the GNU General Public License as published by        #
+#  the Free Software Foundation; either version 2 of the License, or           #
+#  (at your option) any later version.                                         #
+#                                                                              #
+#  This program is distributed in the hope that it will be useful,             #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+#  GNU General Public License for more details.                                #
+#                                                                              #
+#  You should have received a copy of the GNU General Public License           #
+#  along with this program; if not, write to the Free Software                 #
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA   #
+#                                                                              #
+################################################################################
+################################################################################
+################################################################################
+
+
 STAMP=$(date +%Y-%m-%d_%H-%M-%S)
-
-echo -e "DRY RUN without paramateters"
-echo -e "Depending on the size of cluster, this script can run from several seconds to several minutes."
-echo -e ""
-echo -e $STAMP
-echo -e "SUMMARY:"
-echo -e ""
-
 CLIENT="oc"
-
 MUST_PATH=$1
 ORIG_PATH=$(pwd)
 OUTPUT_PATH=$ORIG_PATH/DATA
 OLDER_THAN=30
 
-
 #rm -rf $OUTPUT_PATH
 mkdir -p $OUTPUT_PATH
 
-# PARSER --------------------------------------------------------------------------
-
-print_help() {
-  echo -e "HELP:"
-  echo -e "-f | --force : to actually delete ReplicaSets not in use. (Not implemented)"
-  echo -e "-g | --graph : graph referenced images. (Not implemented)"
-  echo -e ""
-}
 
 PARAMS=""
 while (( "$#" )); do
@@ -40,6 +56,15 @@ while (( "$#" )); do
       if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
         CLIENT="kubectl"
         ETCD_NS="kube-system"
+        shift 2
+      else
+        echo "Error: Argument for $1 is missing" >&2
+        exit 1
+      fi
+      ;;
+    -o|--olderthan)
+      if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
+        OLDER_THAN=$2
         shift 2
       else
         echo "Error: Argument for $1 is missing" >&2
@@ -60,11 +85,31 @@ done
 # set positional arguments in their proper place
 eval set -- "$PARAMS"
 
-#---------------------------------------------------------------------------------
+# FUNCTIONS
 
-oc get ns |grep -v openshift|grep -v kube|grep -v default > $OUTPUT_PATH/ns.log
-oc get ns > $OUTPUT_PATH/ns_all.log
+print_help() {
+  echo -e "DRY RUN without paramateters"
+  echo -e "Depending on the size of cluster, this script can run from several seconds to several minutes."
+  echo -e ""
+  echo -e $STAMP
+  echo -e "SUMMARY:"
+  echo -e ""
+  echo -e "HELP:"
+  # echo -e "-f | --force : to actually delete ReplicaSets not in use. (Not implemented)"
+  # echo -e "-g | --graph : graph referenced images. (Not implemented)"
+  echo -e "-o | --olderthan : print all unused deployments and replicasets, older than X days"
+  echo -e ""
+}
 
+namespaces() {
+  echo -e ""
+  echo -e "[NS]"
+  oc get ns |grep -v openshift|grep -v kube|grep -v default > $OUTPUT_PATH/ns.log
+  oc get ns > $OUTPUT_PATH/ns_all.log
+  echo -e ""
+  echo -e "NS: there are together $(cat $OUTPUT_PATH/ns_all.log|wc -l) namespaces."
+  echo -e "NS: there are $(cat $OUTPUT_PATH/ns.log|wc -l) non-openshift namespaces."
+}
 
 imagestreams() {
   echo -e ""
@@ -96,7 +141,8 @@ replicasets() {
   echo -e "[REPLICASETS]"
   $CLIENT get replicasets -A > $OUTPUT_PATH/rs.log
   cat $OUTPUT_PATH/rs.log |grep  -E '0{1}\s+0{1}\s+0{1}'| awk -v OLDER_THAN=$OLDER_THAN '($6*3600) > ( OLDER_THAN * 3600) {print $1, $2, $6 }'|sort -k 3n|uniq > $OUTPUT_PATH/rs_inactiv.log
-  cat $OUTPUT_PATH/rs_inactiv.log|awk -v OLDER_THAN=$OLDER_THAN '($3*3600) > ( OLDER_THAN*3600 ) {print "oc delete rs -n " $1, $2 }' > $OUTPUT_PATH/older_than_${OLDER_THAN}days.sh
+  cat $OUTPUT_PATH/rs_inactiv.log|grep -v openshift|awk -v OLDER_THAN=$OLDER_THAN '($3*3600) > ( OLDER_THAN*3600 ) {print "oc delete rs -n " $1, $2 }' > $OUTPUT_PATH/older_than_${OLDER_THAN}days.sh
+  cat $OUTPUT_PATH/rs_inactiv.log|grep openshift|awk -v OLDER_THAN=$OLDER_THAN '($3*3600) > ( OLDER_THAN*3600 ) {print "oc delete rs -n " $1, $2 }' > $OUTPUT_PATH/older_ocp_than_${OLDER_THAN}days.sh
   cat $OUTPUT_PATH/rs.log |grep  -E '0{1}\s+0{1}\s+0{1}'| awk '{print "-n " $1, $2}'|sort|uniq > $OUTPUT_PATH/rs_inactive.log
   #cat rs.log |grep  -E '0{1}\s+0{1}\s+0{1}'| awk '{print "oc describe rs -n " $1, $2, $6, ($6*3600) }'|sort -k 7n|uniq
   $CLIENT get rs -A  -o jsonpath="{..image}" | tr -s '[[:space:]]' '\n' | sort | uniq > $OUTPUT_PATH/rs_images.log
@@ -152,27 +198,29 @@ list_all() {
   echo -e ""
 }
 
+diffing() {
+  # echo -e "diffing.."
+  # Print images not used by any pod
+  
+  awk 'NR == FNR{ a[$0] = 1;next } !a[$0]' $OUTPUT_PATH/pod_images.log $OUTPUT_PATH/is_images.log > $OUTPUT_PATH/podis.log
+  
+  # Print images not used by any Deployment
+  
+  awk 'NR == FNR{ a[$0] = 1;next } !a[$0]' $OUTPUT_PATH/deploy.log $OUTPUT_PATH/is_images.log > $OUTPUT_PATH/depis.log
+  awk 'NR == FNR{ a[$0] = 1;next } !a[$0]' $OUTPUT_PATH/depis.log $OUTPUT_PATH/podis.log > $OUTPUT_PATH/EXCESS.log
+}
 
+# MAIN
 
+print_help
+namespaces
 imagestreams
 pods
 deployments
 replicasets
 jobs
-
 list_all
-
-
-# echo -e "diffing.."
-# Print images not used by any pod
-
-awk 'NR == FNR{ a[$0] = 1;next } !a[$0]' $OUTPUT_PATH/pod_images.log $OUTPUT_PATH/is_images.log > $OUTPUT_PATH/podis.log
-
-# Print images not used by any Deployment
-
-awk 'NR == FNR{ a[$0] = 1;next } !a[$0]' $OUTPUT_PATH/deploy.log $OUTPUT_PATH/is_images.log > $OUTPUT_PATH/depis.log
-awk 'NR == FNR{ a[$0] = 1;next } !a[$0]' $OUTPUT_PATH/depis.log $OUTPUT_PATH/podis.log > $OUTPUT_PATH/EXCESS.log
-
+diffing
 
 echo -e "Done"
 
