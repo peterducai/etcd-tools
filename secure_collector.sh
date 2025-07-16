5369ef1bd4153245a71984ca8a1f144bca092f9d
Peter Ducai <32168685+peterducai@users.noreply.github.com>
2025-07-16 12:11:26 +0200

    new messages

--- secure_collector.sh
+++ secure_collector.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-PWD="/home/pducai/Downloads/must-gather.local.3496992070518267793"
+#PWD="/home/pducai/Downloads/must-gather.local.3496992070518267793"
 
 WARNINGS=(
     'apply request took too long'
     'leader changed'
+    'nexpected watch close'
+    'context deadline exceeded'
+    'error on the server'
 )
 
 #grep -inR "sample"
 
-cd $PWD
+# cd $PWD
+cd $1
+
 for str in "${WARNINGS[@]}"; do
   echo -e ""
   echo -e "[$str] -----------------"
