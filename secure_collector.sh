34f8511576728573595035114f64aeef182d4d8b
Peter Ducai <32168685+peterducai@users.noreply.github.com>
2025-07-16 16:04:51 +0200

    print only 10 lines

--- secure_collector.sh
+++ secure_collector.sh
@@ -20,7 +20,7 @@ cd $1
 for str in "${WARNINGS[@]}"; do
   echo -e ""
   echo -e "[$str] -----------------"
-  grep -cinR "$str" | grep -v ':0$' |sort -t':' -n -k2 | tac
+  grep -cinR "$str" | grep -v ':0$' |sort -t':' -n -k2 | tac |head -10
   #grep -cinR "$str" | grep -v ':0$' | grep -Eo '[0-9]+$'
 
   # if [[ $a -gt 50 ]]; then
