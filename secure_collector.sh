7e67a49ff8ed8ef3311078dfe4929a9387953d5d
Peter Ducai <32168685+peterducai@users.noreply.github.com>
2025-07-17 14:24:00 +0200

    timestamps

--- secure_collector.sh
+++ secure_collector.sh
@@ -1,29 +1,43 @@
-#!/bin/bash
-
-LOWLIMIT=1000
+#!/bin/bash 
 
 WARNINGS=(
     'apply request took too long'
     'leader changed'
-    'nexpected watch close'
+    'failed to send out heartbeat on time'
+    'overloaded likely from slow disk'
+    # 'clock difference'
+    # 'Clock not synchronising'
+    'clock skew'
+    'process'
+    'buffer'
+    'unexpected watch close'
     'context deadline exceeded'
     'error on the server'
     'timeout'
     'failed'
+    'forbidden'
+    # 'driver not found'
+    'failed to get'
+    'failed to download'
+    'failed to retrieve a role for a rolebinding'
+    'Failed to find app'
 )
 
-#grep -inR "sample"
-
-# cd $PWD
 cd $1
 
 for str in "${WARNINGS[@]}"; do
+  FILEARR=()
   echo -e ""
   echo -e "[$str] -----------------"
-  grep -cinR "$str" | grep -v ':0$' |sort -t':' -n -k2 | tac |head -10
-  #grep -cinR "$str" | grep -v ':0$' | grep -Eo '[0-9]+$'
-
-  # if [[ $a -gt 50 ]]; then
-  #   #...
-  # fi
+  echo -e ""
+  # grep -cinR "$str" | grep -v ':0$' |sort -t':' -n -k2 | tac |head -10
+  FILEARR=$(grep -cinR "$str" | grep -v ':0$'| grep ".log:"| sort -t':' -n -k2 | tac|head -10)                         #| grep -E -o ":[[:digit:]]+" 
+  readarray -t FIL <<< $FILEARR
+  for i in "${FIL[@]}"
+  do
+    j=${i%:*}
+    LASTSEEN=$(cat $j |grep "$str" |tail -1| head -c 10)
+    echo -e "$i  last seen on $LASTSEEN"
+  done
+  echo -e ""  
 done
